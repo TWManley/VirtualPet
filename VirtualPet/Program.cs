6addd6724d3cd02fdaf2e8df9d92d99d241de650
twmanley <tmanley1@yahoo.com>
2018-06-03 23:18:06 -0400

    first commit

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            
+
+
+
+
+
+                virtualpet virtualpet = new virtualpet();
+
+
+                string textToEnter = "Welcome to Virtual Pet ";
+                Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+
+
+
+
+                Console.WriteLine("What type of pet do you want options include Phaserbeast,RazorBeast,Goldfish");
+                string pettype = Console.ReadLine();
+                Console.ReadLine().ToLower();
+
+                if
+
+
+                    (pettype == "PhaserBeast")
+                    Console.WriteLine("You are the proud owner of a PhaserBeast");
+
+                else if
+
+                    (pettype == "LaserBeast")
+                    Console.WriteLine("You are the proud owner of a LaserBeast");
+
+                else if
+
+                    (pettype == "Goldfish")
+                    Console.WriteLine("You got a gold fish");
+
+
+                Console.WriteLine("What is your new pets name?");
+
+                Console.ReadLine();
+
+                Console.WriteLine("What would you like to do options include status,feed,water,takeout,train,fulfill");
+                string userInput = Console.ReadLine();
+                if (userInput == "status")
+                {
+                    virtualpet.status();
+                }
+                else if (userInput == "feed")
+                {
+                    virtualpet.feed();
+                }
+                else if (userInput == "water")
+                {
+                    virtualpet.water();
+                }
+                else if (userInput == "takeout")
+                {
+                    virtualpet.takeout();
+
+                }
+                else if (userInput == "train")
+                {
+                    virtualpet.train();
+                }
+                Console.WriteLine("What would you like to do next?");
+                userInput = Console.ReadLine();
+                
+                
+                
+
+                
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }   
+    }
+}
---
+++ VirtualPet/Virtual Pet.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class virtualpet
+    {   //fields
+        //
+        public string petsName;
+        public string petType;
+        public int hunger;
+        public int thirst;
+        public int releive;
+        public int energyLevel;
+        public int needs;
+
+
+        //constructor
+        public virtualpet()
+
+        {
+
+
+        }
+        
+        //loaded constructor
+        public virtualpet( string petType, string petsName)
+        {
+            
+            this.petType = petType;
+            this.petsName = petsName;
+
+        }
+        public virtualpet(int hunger, int thirst, int releive, int energyLevel, int needs)
+        {
+            this.hunger = hunger;
+            this.thirst = thirst;
+            this.releive = releive;
+            this.energyLevel = energyLevel;
+            this.needs = needs;
+
+        }
+        public string userInput
+        {
+            get { return this.userInput; }
+            set { this.userInput = value; }
+
+        }
+        public void feed()
+        {
+            this.hunger = 0;
+            Console.WriteLine("you have fed your pet");
+        }
+        public void water()
+        {
+            this.thirst = 0;
+            Console.WriteLine("you have given your pet water");
+        }
+        public void takeout()
+        {
+            this.releive = 0;
+            Console.WriteLine("you have taken you pet out!!!");
+        }
+        public void train()
+        {
+            this.hunger = -5;
+            this.energyLevel = +5;
+            Console.WriteLine("you have played with the virtualpet but you have also made it hungry");
+
+        }
+        public void fulfill()
+        {
+            this.needs = +10;
+            Console.WriteLine("you helped the pet fulfill their needs");
+            
+        }
+        
+        public void status()//this is used to create the random generation of needs for the pet
+        {
+            Random randomGen = new Random();
+            int randomHunger = randomGen.Next(1, 11);
+            int randomThirst = randomGen.Next(1, 11);
+            int randomReleive = randomGen.Next(1, 11);
+            int randomEnergyLevel = randomGen.Next(1, 11);
+            int randomNeeds = randomGen.Next(1, 11);
+            this.needs += randomNeeds;
+            this.energyLevel += randomEnergyLevel;
+            this.hunger += randomHunger;
+            this.thirst += randomThirst;
+            Console.WriteLine("Current needs are {0} the current energy level is {1}current hunger level is {2} the level of thirst is{3} the need for releif is {4}  ", this.needs, this.energyLevel, this.hunger,
+            this.thirst, this.releive);
+
+        }
+        
+        
+            
+            public void Hunger() 
+        {
+            if (hunger < 10)
+            {
+                Console.WriteLine("Would you like to feed your pet?");
+
+            }
+        }
+    }
+}
